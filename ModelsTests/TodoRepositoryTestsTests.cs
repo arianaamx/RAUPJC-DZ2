9296f1f9e8aef0621d159a58c25bf848d0952b77
arianaamx <ariana.milasincic@gmail.com>
2016-10-31 10:51:33 +0100

    Add files via upload

---
+++ ModelsTests/TodoRepositoryTestsTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zadatak1Tests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using Repositories;
+
+namespace Zadatak1Tests.Tests
+{
+    [TestClass()]
+    public class TodoRepositoryTestsTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddingNullToDatabaseThrowsException()
+        {
+            ITodoRepository repository = new TodoRepository();
+            repository.Add(null);
+        }
+        [TestMethod]
+        public void AddingItemWillAddToDatabase()
+        {
+            ITodoRepository repository = new TodoRepository();
+            var todoItem = new TodoItem(" Groceries ");
+            repository.Add(todoItem);
+            Assert.AreEqual(1, repository.GetAll().Count);
+            Assert.IsTrue(repository.Get(todoItem.Id) != null);
+        }
+        [TestMethod]        [ExpectedException(typeof(DuplicateTodoItemException))]
+        public void AddingExistingItemWillThrowException()
+        {
+            ITodoRepository repository = new TodoRepository();
+            var todoItem = new TodoItem(" Groceries ");
+            repository.Add(todoItem);
+            repository.Add(todoItem);
+        }
+    }
+}
